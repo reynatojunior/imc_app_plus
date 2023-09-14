5d1a3b82973efb38a722661e02bd57e5b5247edf
reynatojunior <reynato.junior@hotmail.com>
2023-09-14 07:47:37 -0300

    update
    Calculadora IMC PLUS pronta

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,135 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(CalcularIMC());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class CalcularIMC extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Calculadora de IMCPLUS',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.lightGreen,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: IMCCalculator(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class IMCCalculator extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _IMCCalculatorState createState() => _IMCCalculatorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _IMCCalculatorState extends State<IMCCalculator> {
+  List<IMCData> imcDataList = [];
 
-  void _incrementCounter() {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  void calculeIMC() {
+    double peso = double.parse(weightController.text);
+    double altura =
+        double.parse(heightController.text) / 100; // altura de cm para metros
+
+    double imc = peso / (altura * altura);
+
+    IMCData imcData = IMCData(peso: peso, altura: altura, imc: imc);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      imcDataList.add(imcData);
     });
+
+    // Limpa os campos de texto após o cálculo
+    weightController.clear();
+    heightController.clear();
+  }
+
+  String getClassificacao(double imc) {
+    if (imc < 16) {
+      return 'Magreza grave';
+    } else if (imc < 17 && imc > 16) {
+      return 'Magreza moderada';
+    } else if (imc < 18.5 && imc > 17) {
+      return 'Magreza leve';
+    } else if (imc < 25 && imc > 18.5) {
+      return 'Saudável';
+    } else if (imc < 30 && imc > 25) {
+      return 'Sobrepeso';
+    } else if (imc < 35 && imc > 30) {
+      return 'Obesidade Grau I';
+    } else if (imc < 40 && imc > 35) {
+      return 'Obesidade Grau II';
+    } else {
+      return 'Obesidade Grau III';
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Calculadora de IMCPLUS'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: EdgeInsets.all(16),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            TextField(
+              controller: weightController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Peso (kg)',
+              ),
             ),
+            TextField(
+              controller: heightController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Altura (cm)',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: calculeIMC,
+              child: Text('Calcular IMC'),
+            ),
+            SizedBox(height: 16),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Histórico de IMC:',
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: imcDataList.length,
+                itemBuilder: (context, index) {
+                  IMCData imcData = imcDataList[index];
+                  String classificacao = getClassificacao(imcData.imc);
+                  return ListTile(
+                    title: Text('IMC: ${imcData.imc.toStringAsFixed(2)}'),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                            'Peso: ${imcData.peso} kg, Altura: ${imcData.altura} m'),
+                        Text('Classificação: $classificacao'),
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class IMCData {
+  final double peso;
+  final double altura;
+  final double imc;
+
+  IMCData({required this.peso, required this.altura, required this.imc});
 }
